c64d75a83d86d263910627898e95e1d88b3a8ebd
Bastien <record.bastien@gmail.com>
2024-11-01 11:56:51 +0100

    feat: Init bodyPart and 5 exercises

--- ProGym/ProGymApp.swift
+++ ProGym/ProGymApp.swift
@@ -13,6 +13,10 @@ struct ProGymApp: App {
 		return container
 	}()
 	
+	init() {
+		populateStaticData()
+	}
+	
 	var body: some Scene {
         WindowGroup {
             ContentView()
@@ -22,4 +26,54 @@ struct ProGymApp: App {
 				)
         }
     }
+	
+	func populateStaticData(){
+		let context = persistenceContainer.viewContext
+		
+		let request: NSFetchRequest<BodyPart> = BodyPart.fetchRequest()
+		let count = (try? context.count(for: request)) ?? 0
+		guard count == 0 else { return }
+		
+		let amrs = BodyPart(context: context)
+		amrs.name = "AMRS"
+		let back = BodyPart(context: context)
+		back.name = "BACK"
+		let chest = BodyPart(context: context)
+		chest.name = "CHEST"
+		let legs = BodyPart(context: context)
+		legs.name = "LEGS"
+		let shoulders = BodyPart(context: context)
+		shoulders.name = "SHOULDERS"
+		
+		let dumbbellBicepsCurl = Exercise(context: context)
+		dumbbellBicepsCurl.name = "Dumbbell Biceps Curl"
+		dumbbellBicepsCurl.pictureUrl = "dumbbell-biceps-curl.png"
+		dumbbellBicepsCurl.bodyPart = amrs
+		
+		let barbellRowing = Exercise(context: context)
+		barbellRowing.name = "Barbell Rowing"
+		barbellRowing.pictureUrl = "barbell-rowing.png"
+		barbellRowing.bodyPart = back
+		
+		let benchPress = Exercise(context: context)
+		benchPress.name = "Bench Press"
+		benchPress.pictureUrl = "bench-press.png"
+		benchPress.bodyPart = chest
+		
+		let squat = Exercise(context: context)
+		squat.name = "Squat"
+		squat.pictureUrl = "squat.png"
+		squat.bodyPart = legs
+		
+		let dumbbellShoulderPress = Exercise(context: context)
+		dumbbellShoulderPress.name = "Dumbbell Shoulder Press"
+		dumbbellShoulderPress.pictureUrl = "dumbbell-shoulder-press.png"
+		dumbbellShoulderPress.bodyPart = shoulders
+		
+		do {
+			try context.save()
+		} catch {
+			print("Error saving data: \(error)")
+		}
+	}
 }
